d24a11da5ce8559eb2dec2a5f526b1e89c87f96a
Rafael Berral Soler <rafabs@outlook.com>
2018-03-01 15:44:23 +0100

    Some corrections made on the data loading function

--- preprocess2.py
+++ preprocess2.py
@@ -2,29 +2,29 @@ from preprocess import getDims, getSerie, getValues
 import cv2
 import glob
 import numpy as np
+import random
 
-def getFace2(img, off_perc, dir):
+def getFace2(img, alter = 0):
 
     [centerx, centery, width, height] = getDims(img)
-
-    if dir == 1 or dir == 2:
-        offset = int((off_perc * width)/100)
-    else:
-        offset = int((off_perc * height)/100)
 
     xcorner = (centerx - int(width / 2))
     ycorner = (centery - int(height / 2))
 
-    if(dir == 1):
-        pic = cv2.imread(img, cv2.IMREAD_GRAYSCALE)[ycorner:(ycorner + height), xcorner:(xcorner + width - offset)]
-    elif(dir == 2):
-        pic = cv2.imread(img, cv2.IMREAD_GRAYSCALE)[ycorner:(ycorner + height), xcorner:(xcorner + width + offset)]
-    elif(dir == 3):
-        pic = cv2.imread(img, cv2.IMREAD_GRAYSCALE)[ycorner:(ycorner + height + offset), xcorner:(xcorner + width)]
-    elif(dir == 4):
-        pic = cv2.imread(img, cv2.IMREAD_GRAYSCALE)[ycorner:(ycorner + height - offset), xcorner:(xcorner + width)]
-    else:
+    if(alter == 0):
         pic = cv2.imread(img, cv2.IMREAD_GRAYSCALE)[ycorner:(ycorner + height), xcorner:(xcorner + width)]
+    else:
+
+        x_offset = int((width * random.uniform(5, 10)) / 100)
+        y_offset = int((height * random.uniform(5, 10)) / 100)
+
+        if random.randint(0, 1) == 1:
+            x_offset = x_offset * -1
+
+        if random.randint(0, 1) == 1:
+            y_offset = y_offset * -1
+
+        pic = cv2.imread(img, cv2.IMREAD_GRAYSCALE)[ycorner:(ycorner + height + y_offset), xcorner:(xcorner + width + x_offset)]
 
     return pic
 
@@ -51,34 +51,11 @@ def getData2(width, height):
 
             print("Cargando imagen %d..." % facecount)
 
-            # Obtener imagen original
-
-            pic = getFace2(img, 0, 0)
-            pic_eq = cv2.equalizeHist(pic)
-            pic = cv2.resize(pic, (width, height))
-            pic_eq = cv2.resize(pic_eq, (width, height))
-            flat1 = pic.flatten() / 255.0
-            flat2 = pic_eq.flatten() / 255.0
-            values = getValues(img)
-
-            if getSerie(img) == 1:
-                serie1[i1] = flat1
-                serie1[i1 + 1] = flat2
-                values1[i1] = values
-                values1[i1 + 1] = values
-                i1 += 2
-            else:
-                serie2[i2] = flat1
-                serie2[i2 + 1] = flat2
-                values2[i2] = values
-                values2[i2 + 1] = values
-                i2 += 2
-
             # Obtener variaciones de la imagen.
 
-            for i in range(1, 5):
+            for i in range(0, 5):
 
-                pic = getFace2(img, 5, i)
+                pic = getFace2(img, i)
                 pic_eq = cv2.equalizeHist(pic)
                 pic = cv2.resize(pic, (width, height))
                 pic_eq = cv2.resize(pic_eq, (width, height))
